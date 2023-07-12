effaf4a8f949ae643797364b7fd3ab3b4721ab4a
A-Kimpo <ak.kimpo@gmail.com>
2023-07-12 17:57:36 +0500

    feat: get data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import axios from 'axios';
+import routes from './routes';
+import { useEffect, useState } from 'react';
+import Frame from './components/frame/Frame';
 
 const App = () => {
+  const [dates, setDates] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(routes.calendarPath());
+
+      setDates({ ...data });
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Frame dates={dates} />
+    </div>
   );
 };
 
