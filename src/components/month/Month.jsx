b93e50aa320b4c9bfe3c7b326fdfd951d6d2eb5d
A-Kimpo <ak.kimpo@gmail.com>
2023-07-12 18:16:57 +0500

    fix: add React fragment

--- src/components/month/Month.jsx
+++ src/components/month/Month.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import './Month.css';
 
 const Month = (props) => {
   return (
-    <div className="month">{props.months.map((m) => <p>{m}</p>)}</div>
+    <div className="month">{props.months.map((m, i) => <React.Fragment key={i}><p>{m}</p></React.Fragment>)}</div>
   );
 };
 
