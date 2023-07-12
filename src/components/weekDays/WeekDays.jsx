bb933c4a44068c8de587ef50b0256f3c332e5aff
A-Kimpo <ak.kimpo@gmail.com>
2023-07-12 17:59:01 +0500

    feat: add weekdays component

---
+++ src/components/weekDays/WeekDays.jsx
@@ -0,0 +1,13 @@
+import './WeekDays.css';
+
+const WeekDays = () => {
+  return (
+    <div className="week__days">
+      <p className="day-name">Пн</p>
+      <p className="day-name">Ср</p>
+      <p className="day-name">Пт</p>
+    </div>
+  );
+};
+
+export default WeekDays;
